0e2dc8aaa8bb50f42d0cd3e30cb70e2ca0d00737
DANIIL_PKHAKADZE <s4101606@student.rmit.edu.au>
2026-09-09 21:15:55 +1000

    defined a pydantic model for a single sensor reading

--- backendAPI/main.py
+++ backendAPI/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import datetime
+
+class Record(BaseModel):
+    timestamp: datetime
+    temperature: float
+    moisture_percent: float
+    o2_percent: float
+    co2_percent: float
+    nh3_ratio: float
 
 app = FastAPI()
 
