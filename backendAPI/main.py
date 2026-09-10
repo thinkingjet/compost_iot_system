24ef798a642c1cb37c3fca8f51c5183ec740844c
devvotel <s4101606@student.rmit.edu.au>
2026-09-10 14:15:01 +1000

    working on the API database connection

--- backendAPI/main.py
+++ backendAPI/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import datetime
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 class Record(BaseModel):
     timestamp: datetime.datetime
@@ -12,6 +16,13 @@ class Record(BaseModel):
 
 app = FastAPI()
 
+POSTGRES_DB_URL = "postgresql+psycopg2://postgres:devpassword@localhost:5432/compostiq"
+db_engine = create_engine(POSTGRES_DB_URL)
+with db_engine.connect() as connection:
+    result = connection.execute(text("SELECT count(*) FROM USERS"))
+    for row in result:
+        print(row)
+
 
 @app.get("/")
 async def root():
@@ -20,3 +31,4 @@ async def root():
 @app.post("/records")
 async def send_records (readings: list[Record]):
     return readings
+
